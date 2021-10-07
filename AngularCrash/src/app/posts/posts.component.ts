d63c511adb625de64f49d430cc687dfb9a32e828
johnvillar41 <johndanielvillar123@gmail.com>
2021-10-07 15:05:16 +0800

    Added posts

--- AngularCrash/src/app/posts/posts.component.ts
+++ AngularCrash/src/app/posts/posts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/services/data.service';
 
 @Component({
   selector: 'app-posts',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostsComponent implements OnInit {
 
-  constructor() { }
+  public posts!: any;
+  constructor(private dataService: DataService) {
+
+  }
 
   ngOnInit(): void {
+    this.dataService.getPosts().subscribe(
+      data => this.posts = data
+    );
   }
 
 }
